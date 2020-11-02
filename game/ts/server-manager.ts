4d42b9f90d21d5618bf94b270ebf251c5d9a6003
Nv7 <junk4Nv7@gmail.com>
2020-11-01 16:54:30 -0800

    Switched servers

--- game/ts/server-manager.ts
+++ game/ts/server-manager.ts
@@ -16,7 +16,7 @@ export const builtInOfficialServers = [
 export const builtInThirdPartyServers = [
   'https://elemental.hparcells.tk',
   'https://dev.elemental5.net/api',
-  'https://cors-anywhere.herokuapp.com/https://elementalserver-8c6d0.web.app/elemental.json',
+  'https://nv7-github.github.io/elemental.json',
   // 'https://e4api.ledomsoft.com',
 ];
 export const allBuiltInServers = [
