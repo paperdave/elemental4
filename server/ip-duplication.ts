90de71d585957246a24185110b7fad5ee65be577
dave caruso <dave@davecode.me>
2020-11-22 16:48:21 -0500

    modify ip scoring system

--- server/ip-duplication.ts
+++ server/ip-duplication.ts
@@ -7,8 +7,8 @@ export async function lookupIpCheck(ip: string) {
   if (ip.startsWith('0.')) return true;
   const url = `https://ipqualityscore.com/api/json/ip/${IP_DUPLICATION_KEY}/${ip}?strictness=0&allow_public_access_points=true&fast=true&lighter_penalties=true&mobile=true`
   const result = await fetch(url).then(x => x.json());
-  if('vpn' in result) {
-    return !(result['fraud_score'] > 60 || result['vpn']);
+  if('fraud_score' in result) {
+    return !(result.mobile ? result.fraud_score > 85 : (result.fraud_score > 70 || result.vpn));
   } else {
     throw new Error('Cannot call IP Duplication API.');
   }
