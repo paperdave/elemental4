d2ea54b3734d2e55125494f05431e3636d6828bb
Nv7 <junk4Nv7@gmail.com>
2020-11-22 17:11:00 -0800

    Creating elements

--- shared/api/nv7/suggestions.ts
+++ shared/api/nv7/suggestions.ts
@@ -4,7 +4,6 @@ import firebase from "firebase/app";
 import 'firebase/database';
 import 'firebase/firestore';
 import { NV7ElementalAPI } from "./nv7";
-import { Element } from "./types";
 
 async function getSuggestionCombo(api: NV7ElementalAPI, elem1: string, elem2: string): Promise<string[]> {
   let resp = await fetch(api.prefix + "suggestion_combos/" + encodeURIComponent(elem1) + "/" + encodeURIComponent(elem2))
@@ -56,10 +55,6 @@ async function upvoteSuggestion(id: string, api: NV7ElementalAPI, request: Sugge
   let resp = await fetch(api.prefix  + "up_suggestion/" + id + "/" + api.uid);
   var text = await resp.text();
   if (text == "create") {
-    var existing = await getSuggestion(api, id);
-    await firebase.database().ref("/suggestions/" + id).remove();
-    await firebase.database().ref("/suggestionMap/" + parents[0] + "/" + parents[1]).remove();
-
     let commentData = await api.ui.prompt({
       title: "Confirm Creator Mark",
       text: "Enter below what you want to use for your creator mark. You are responsible for the creation of this element!"
@@ -68,47 +63,23 @@ async function upvoteSuggestion(id: string, api: NV7ElementalAPI, request: Sugge
     if (commentData) {
       comment = commentData;
     }
-
-    // Created element
-    var element: Element = {
-      name: request.text,
-      color: existing.color.base + "_" + existing.color.saturation + "_" + existing.color.lightness,
-      creator: existing.creator,
-      pioneer: api.saveFile.get("email", "anonymous"),
-      parents: parents,
-      comment: comment,
-      createdOn: new Date().getTime()
-    }
-    // New Recent Combination
-    var recCombo: RecentCombination = {
-      recipe: parents as [string, string],
-      result: request.text,
+    resp = await fetch(api.prefix + "create_suggestion/" + encodeURIComponent(parents[0]) + "/" + encodeURIComponent(parents[1]) + "/" + encodeURIComponent(id) + "/" + encodeURIComponent(comment) + "/" + encodeURIComponent(api.saveFile.get("email", "anonymous")));
+    text = await resp.text();
+    if (text != "") {
+      await api.ui.alert({
+        "text": text,
+        "title": "Error",
+        "button": "Ok",
+      });
+      return {
+        suggestType: "failed"
+      }
     }
 
-    var existingCombos: RecentCombination[] = await api.getRecentCombinations(30);
-    if (!existingCombos) {
-      existingCombos = [];
+    return {
+      suggestType: "vote",
+      newElements: [id]
     }
-    existingCombos.push(recCombo);
-    await firebase.database().ref("/recent/").update(existingCombos);
-
-    // Create Element
-    await firebase.firestore().collection("elements").doc(element.name).set(element);
-
-    return new Promise<SuggestionResponse>(async (resolve, reject) => {
-      firebase.firestore().collection("combos").doc(parents[0]).get().then(async (snapshot) => {
-        var data = snapshot.data();
-        if (!data) {
-          data = {};
-        }
-        data[parents[1]] = element.name;
-        await firebase.firestore().collection("combos").doc(parents[0]).set(data);
-        resolve({
-          suggestType: "vote",
-          newElements: [element.name]
-        })
-      })
-    })
   }
   if (text != "") {
     await api.ui.alert({
