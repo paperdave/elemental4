e74010340939a4814f156ff6df32eb030c89aa7b
Nv7 <junk4Nv7@gmail.com>
2020-11-26 16:52:57 -0800

    Get rid of bug where spams server

--- shared/api/nv7/savefile.ts
+++ shared/api/nv7/savefile.ts
@@ -1,10 +1,11 @@
 import {NV7ElementalAPI} from "./nv7";
 
 export async function foundElement(api: NV7ElementalAPI, newElement: string): Promise<void> {
+  var elemData: Element = await api.store.get(newElement);
+  if (!elemData) {
     await fetch(api.prefix + "new_found/" + api.uid + "/" + encodeURIComponent(newElement))
-    return null;
+  }
 }
-
 export async function getFound(api: NV7ElementalAPI): Promise<string[]> {
   let foundResp = await fetch(api.prefix + "get_found/" + api.uid);
   let found = await foundResp.json();
