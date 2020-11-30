11fed136d3b4fe1e9d3159ac3261b0ece84ce3dc
Zolo101 <umadeguy@gmail.com>
2020-11-30 20:12:58 +0000

    Init tree implementation

--- game/ts/tree.ts
+++ game/ts/tree.ts
@@ -1,5 +1,6 @@
 import { Elem } from "../../shared/elem";
 import { getAPI } from "./api";
+import { getTheme } from "./theme";
 
 interface Tree {
   /** The element itself. */
@@ -32,7 +33,73 @@ let canvasRunning = true;
 let canvas: HTMLCanvasElement;
 let ctx: CanvasRenderingContext2D;
 let currentTree: Tree;
+let elementSize = 35;
 
+function addText(text: string, x: number, y: number) {
+  const textToAdd = text.match(/.{1,6}/g);
+  textToAdd.forEach((text, i) => {
+    ctx.fillStyle = "#000"
+    ctx.fillText(text, x, y+(i*14))
+  })
+}
+function makeElement(color: string, text: string, x: number, y: number, cx: number, cy: number) {
+  ctx.fillStyle = color
+  ctx.fillRect(x+cx, y+cy, elementSize, elementSize)
+  addText(text, x+cx, y+cy + 14)
+  ctx.beginPath();
+  ctx.moveTo(x+(elementSize/2),y);
+  ctx.lineTo(x+cx+(elementSize/2),y+cy+(elementSize));
+  ctx.stroke();
+}
+function addParents(tree: Tree, x: number, y: number) {
+  // console.warn(getTheme().colors[parent1element.categoryName])
+  let yChange = 45
+  const parent1Exist = (tree.parent1 !== null) ? true : false
+  const parent2Exist = (tree.parent2 !== null) ? true : false
+  const singleParent = ((parent1Exist || parent2Exist) && !(parent1Exist && parent2Exist)) ? true : false
+
+  if (parent1Exist && singleParent) {
+    makeElement(
+      getTheme().colors[tree.parent1.elem.display.categoryName].color,
+      tree.parent1.elem.display.text,
+      x,y,-30,-yChange
+    )
+    makeElement(
+      getTheme().colors[tree.parent1.elem.display.categoryName].color,
+      tree.parent1.elem.display.text,
+      x,y,30,-yChange
+    )
+    addParents(tree.parent1, x-30,y-yChange)
+  } else if (parent1Exist) {
+    makeElement(
+      getTheme().colors[tree.parent1.elem.display.categoryName].color,
+      tree.parent1.elem.display.text,
+      x,y,-30,-yChange
+    )
+    addParents(tree.parent1, x+25, y-yChange)
+  }
+
+  if (parent2Exist && singleParent) {
+    makeElement(
+      getTheme().colors[tree.parent2.elem.display.categoryName].color,
+      tree.parent2.elem.display.text,
+      x,y,-30,-yChange
+    )
+    makeElement(
+      getTheme().colors[tree.parent2.elem.display.categoryName].color,
+      tree.parent2.elem.display.text,
+      x,y,30,-yChange
+    )
+    addParents(tree.parent2, x+30, y-yChange)
+  } else if (parent2Exist) {
+    makeElement(
+      getTheme().colors[tree.parent2.elem.display.categoryName].color,
+      tree.parent2.elem.display.text,
+      x,y,30,-yChange
+    )
+    addParents(tree.parent2, x+50, y-yChange)
+  }
+}
 function update() {
   if(!canvasRunning) {
     return;
@@ -50,6 +117,13 @@ export async function initTreeCanvas(tree: Tree) {
   canvas.width = 350;
   canvas.height = 400;
   update();
+
+  console.log(currentTree);
+
+  ctx.fillStyle = getTheme().colors[tree.elem.display.categoryName].color
+  ctx.fillRect(175, 350, elementSize, elementSize)
+  addText(currentTree.elem.display.text, 175, 357.5)
+  addParents(currentTree, 175, 350);
 }
 export async function endTreeCanvas() {
   canvasRunning = false;
