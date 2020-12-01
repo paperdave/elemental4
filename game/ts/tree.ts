0d486d8b9883db094dd4eecb1d157212a6b21aa2
Zolo101 <umadeguy@gmail.com>
2020-12-01 21:19:41 +0000

    Pan and Scroll

--- game/ts/tree.ts
+++ game/ts/tree.ts
@@ -9,6 +9,8 @@ interface Tree {
   parent1: Tree | null;
   /** Parent 2. null if no parents OR the parent1 is combined with itself */
   parent2: Tree | null;
+  /** The amount of unique children needed to make the element */
+  children: number;
 }
 
 export async function getElementTree(elem: Elem): Promise<Tree|null> {
@@ -19,12 +21,14 @@ export async function getElementTree(elem: Elem): Promise<Tree|null> {
       parent2: elem.stats.simplestRecipe[1] === elem.stats.simplestRecipe[0]
         ? null
         : await getElementTree(await getAPI().getElement(elem.stats.simplestRecipe[1])),
+      children: 0,
     }
   } else {
     return {
       elem: elem,
       parent1: null,
       parent2: null,
+      children: 0,
     }
   }
 }
@@ -34,7 +38,15 @@ let container: HTMLCanvasElement;
 let ctx: CanvasRenderingContext2D;
 let currentTree: Tree;
 const elementSize = 35;
+const treeCanvasSizeX = 350;
+const treeCanvasSizeY = 400;
+let treePosX = 175;
+let treePosY = 350;
+let scale = 1;
 
+function presolveTree() {
+
+}
 function addText(text: string, x: number, y: number) {
   p5.fill(0)
   p5.textSize(10)
@@ -85,16 +97,37 @@ function addParents(tree: Tree, x: number, y: number) {
     addParents(tree.parent2, x+50, y-yChange)
   }
 }
+function renderTree(x: number, y: number) {
+  p5.fill(p5.color(getTheme().colors[currentTree.elem.display.categoryName].color.toString()))
+  p5.rect(x, y, elementSize, elementSize)
+  addText(currentTree.elem.display.text, x, y+7.5)
+  addParents(currentTree, x, y);
+}
 function setup() {
-  p5.createCanvas(350, 400)
+  p5.createCanvas(treeCanvasSizeX,treeCanvasSizeY)
+  console.log(getTheme().colors[currentTree.elem.display.categoryName].color.toString())
+  renderTree(175, 350);
 }
 function draw() {
+  // Mouse panning... kinda
+  console.log(scale)
+  p5.scale(scale)
+  if (p5.mouseIsPressed) {
+    // console.log("Mouse pressed")
+    // console.log(treePosX,treePosY)
+    p5.background(255)
+    renderTree(treePosX, treePosY);
+  }
+
   // TODO: Mouse panning to move the tree
   /*
   We could very lazily do the whole thing every frame,
   or we could redo the code to only compute the tree once, also
   allowing us to do the spacing thingy i mentioned in the
   discord. I'm probably going to do the latter. ~Zelo101
+
+  Edit: I did it the lazy way, but it doesn't seem to have an
+  affect on performace, so lol ~Zelo101
   */
 }
 
@@ -106,16 +139,22 @@ export async function initTreeCanvas(tree: Tree) {
     p5 = instance
     p5.setup = setup;
     p5.draw = draw;
+    p5.mouseWheel = (event: WheelEvent) => {
+      scale -= event.deltaY / 1000;
+      p5.background(255);
+      renderTree(treePosX, treePosY);
+    }
+    p5.mouseDragged = () => {
+      treePosX += (p5.mouseX - p5.pmouseX) / scale;
+      treePosY += (p5.mouseY - p5.pmouseY) / scale;
+    }
   }, container);
-  console.log(getTheme().colors[tree.elem.display.categoryName].color.toString())
-  p5.fill(p5.color(getTheme().colors[tree.elem.display.categoryName].color.toString()))
-  p5.rect(175, 350, elementSize, elementSize)
-  addText(currentTree.elem.display.text, 175, 357.5)
-  addParents(currentTree, 175, 350);
 }
 export async function endTreeCanvas() {
   if (p5) {
     p5.remove();
   }
+  treePosX = 175;
+  treePosY = 350;
   currentTree = null;
 }
