b3babc4fd9bb50f7ed05325bb837f490fcf201ef
Nv7 <junk4Nv7@gmail.com>
2021-01-29 09:23:21 -0800

    Reading elements UI

--- shared/api/nv7/elements.ts
+++ shared/api/nv7/elements.ts
@@ -1,13 +1,17 @@
 import {Element} from "./types";
-import {Elem} from "../../elem";
+import {Elem, ElementalLoadingUi} from "../../elem";
 import { NV7ElementalAPI } from "./nv7";
+import { getFound } from "./savefile";
 
 export async function getElem(api: NV7ElementalAPI, id: string): Promise<Elem> {
-  var elemData: Element = await api.cache.get(id);
-  if (!elemData) {
-    var req = await fetch(api.prefix + "get_elem/" + encodeURIComponent(id));
-    elemData = await req.json();
-    await api.cache.store(elemData);
+  var elemData: Element = api.elemCache[id];
+  if (!(id in api.elemCache)) {
+    elemData = await api.cache.get(id);
+    if (!elemData) {
+      var req = await fetch(api.prefix + "get_elem/" + encodeURIComponent(id));
+      elemData = await req.json();
+      await api.cache.store(elemData);
+    }
   }
 
   return {
@@ -38,4 +42,25 @@ export async function getCombination(api: NV7ElementalAPI, elem1: string, elem2:
     return [];
   }
   return [comboData.combo];
+}
+
+export async function downloadElems(api: NV7ElementalAPI, ui: ElementalLoadingUi) {
+  ui.status("Loading Savefile", 0)
+  let found = await getFound(api);
+  ui.status("Reading Elements", 0)
+  var id: string;
+  var i: any;
+  for (i in found) {
+    id = found[i];
+    ui.status("Reading Elements", i/found.length);
+    if (!(id in api.elemCache)) {
+      var elemData: Element = await api.cache.get(id);
+      if (!elemData) {
+        var req = await fetch(api.prefix + "get_elem/" + encodeURIComponent(id));
+        elemData = await req.json();
+        await api.cache.store(elemData);
+      }
+      api.elemCache[id] = elemData
+    }
+  }
 }
